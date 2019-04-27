71bd8dab45f0fdf6d2569471472eb54cb5716134
FantBoy <549003954@qq.com>
2019-04-27 16:53:13 +0800

    分组相关接口

--- nodetube_app/views.py
+++ nodetube_app/views.py
@@ -14,54 +14,58 @@ logger = logging.getLogger('django')
 
 
 class QueryGroupList(View):
-
+    @HttpRequestDecorator
     def get(self, request):
-        data = [
-            {
-                "title": "",
-                "createdAt": "",
-                "owner": "",
-                "subDescription": ""
-            }
-        ]
-        print(request.GET.get('name'))
 
         group_list = ProgrameGroup.objects.all()
         groups = {}
         if group_list:
             groups = [group.to_dict() for group in group_list]
+        return groups, False
 
-        return JsonResponse(groups)
 
 class EditGroup(View):
 
+    def getData(self):
+        group_list = ProgrameGroup.objects.all()
+        groups = {}
+        if group_list:
+            groups = [group.to_dict() for group in group_list]
+        return groups
+
     def add(self, request, request_dict):
         try:
-            result = {"ret": 0, "msg": ""}
+            result = {"ret": -1, "msg": ""}
             if 'id' in request_dict:
                 del request_dict['id']
             new_group = ProgrameGroup(**request_dict)
             new_group.save()
+            result['ret'] = 0
         except NuwaApiServerException as e:
             logger.error(e.response_msg)
         except Exception as e:
             logger.error(e)
         finally:
-            return result
+            if -1 == result['ret']:
+                return result
+            else:
+                return self.getData()
     # def get(self, request):
     #     request_dict = request.GET
     #     return self.do(request, request_dict)
 
     @HttpRequestDecorator
     def post(self, request):
-        request_dict = request.POST.dict()
+        # antd post的表单数据在body，不在requet.POST
+        #request_dict = request.POST.dict()
+        request_dict = json.loads(request.body)
         method = request_dict.get('method', None)
 
         if not method:
-            errmsg = "post data do net has mothed: ", json.dump(request_dict)
+            errmsg = "post data do not has mothed: ", json.dump(request_dict)
             logger.error(errmsg)
             return {"ret": -1, "msg": errmsg}
             #raise NuwaApiServerException(PostParamsError)
         del request_dict['method']
         if 'post' == method:
-            return self.add(request, request_dict)
+            return self.add(request, request_dict), False
