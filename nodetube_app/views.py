13ba3e64cc2bc02a3ab6b19d1cf02deb1c1ab887
FantBoy <549003954@qq.com>
2019-04-27 17:43:44 +0800

    分组列表相关api：新增、编辑和删除 接口

--- nodetube_app/views.py
+++ nodetube_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from django.views.generic import View
+from django.core.exceptions import ObjectDoesNotExist
 from common.DecoratorTools.HttpRequest import HttpRequestDecorator
 from common.ErrorModule.ErrorException import NuwaApiServerException
 from common.ErrorModule.ErrorCode import *
@@ -10,8 +11,6 @@ import json
 
 import logging 
 logger = logging.getLogger('django')
-
-
 
 class QueryGroupList(View):
     @HttpRequestDecorator
@@ -23,7 +22,6 @@ class QueryGroupList(View):
             groups = [group.to_dict() for group in group_list]
         return groups, False
 
-
 class EditGroup(View):
 
     def getData(self):
@@ -33,16 +31,57 @@ class EditGroup(View):
             groups = [group.to_dict() for group in group_list]
         return groups
 
-    def add(self, request, request_dict):
+    def add(self, request_dict):
+        result = {"ret": -1, "msg": ""}
         try:
-            result = {"ret": -1, "msg": ""}
-            if 'id' in request_dict:
-                del request_dict['id']
+
             new_group = ProgrameGroup(**request_dict)
             new_group.save()
             result['ret'] = 0
         except NuwaApiServerException as e:
             logger.error(e.response_msg)
+        except Exception as e:
+            logger.error(e)
+        finally:
+            if -1 == result['ret']:
+                return result
+            else:
+                return self.getData()
+
+    def delete(self, id, request_dict):
+        result = {"ret": -1, "msg": ""}
+        try:
+            group = ProgrameGroup.objects.get(id=id)
+            group.delete()
+            result['ret'] = 0
+        except NuwaApiServerException as e:
+            logger.error(e.response_msg)
+        except ObjectDoesNotExist as e:
+            result['msg'] = """this group id {} is not exists in group_list""".format(id)
+            logger.error(result['msg'])
+        except Exception as e:
+            logger.error(e)
+        finally:
+            if -1 == result['ret']:
+                return result
+            else:
+                return self.getData()
+
+    def update(self, id, request_dict):
+        result = {"ret": -1, "msg": ""}
+        try:
+
+            group = ProgrameGroup.objects.get(id=id)
+            group.group_name = request_dict['group_name']
+            group.desc = request_dict['desc']
+            # group.modify_time = request_dict['modify_time'] #column设置了自动更新时间
+            group.save()
+            result['ret'] = 0
+        except NuwaApiServerException as e:
+            logger.error(e.response_msg)
+        except ObjectDoesNotExist as e:
+            result['msg'] = """this group id {} is not exists in group_list""".format(id)
+            logger.error(result['msg'])
         except Exception as e:
             logger.error(e)
         finally:
@@ -60,12 +99,19 @@ class EditGroup(View):
         #request_dict = request.POST.dict()
         request_dict = json.loads(request.body)
         method = request_dict.get('method', None)
+        id = request_dict.get('id', None)
 
-        if not method:
-            errmsg = "post data do not has mothed: ", json.dump(request_dict)
+        if not method or None == id:
+            errmsg = """post data do not has mothed or id: {}""".format(json.dumps(request_dict))
             logger.error(errmsg)
             return {"ret": -1, "msg": errmsg}
-            #raise NuwaApiServerException(PostParamsError)
+
         del request_dict['method']
+        del request_dict['id']
+
         if 'post' == method:
-            return self.add(request, request_dict), False
+            return self.add(request_dict), False
+        elif 'delete' == method:
+            return self.delete(id, request_dict), False
+        elif 'update' == method:
+            return self.update(id, request_dict), False
